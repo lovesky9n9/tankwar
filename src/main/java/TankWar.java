9e3c907da325c55147092a842367b504ead34278
Deilord <fds545466@gmail.com>
2020-07-28 21:36:24 +0800

    新增顯示視窗

---
+++ src/main/java/TankWar.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class TankWar {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.add(new GameClient());//可設定視窗大小，為空則為預設大小
+        frame.setTitle("TankWar");//視窗title名稱
+        frame.setVisible(true);//顯示視窗
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//視窗關閉程式停止
+        frame.pack();//畫面展開
+    }
+}
