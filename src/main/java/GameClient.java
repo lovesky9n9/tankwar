2b6e0c3bab3f50ec1ef1fd398c1f4e496fea2511
Deilord <fds545466@gmail.com>
2020-07-28 21:36:48 +0800

    新增視窗大小設定

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,5 +1,16 @@
-public class GameClient {
-    public static void main(String[] args) {
-        System.out.println("TankWar");
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+    private int screenWidth;
+    private int screenHeight;
+    //預設視窗大小
+    GameClient() {
+        this(1027,768);
+    }
+    public GameClient(int screenWidth, int screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
     }
 }
