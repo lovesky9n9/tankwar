b84a12ea13158a48e79d82baa7af1e3d5b4764e2
Deilord <fds545466@gmail.com>
2020-07-30 19:13:21 +0800

    新增坦克圖片

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -4,13 +4,21 @@ import java.awt.*;
 public class GameClient extends JComponent {
     private int screenWidth;
     private int screenHeight;
-    //預設視窗大小
-    GameClient() {
-        this(1027,768);
+
+    GameClient() {//預設視窗大小
+        this(1024, 768);
     }
+
     public GameClient(int screenWidth, int screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
-        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        //super.paintComponent(g);
+        g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),
+                488, 360, null);
     }
 }
